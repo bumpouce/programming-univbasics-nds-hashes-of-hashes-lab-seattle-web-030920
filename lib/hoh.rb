571c88d2179e9dc28b7445ee2cf2a473af2da819
Christine Bumpous <bumpouce@gmail.com>
2020-01-22 01:00:28 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -48,7 +48,7 @@ def naming_system
   
   
   PORTION_3[:sub_category][:sub_category] =  PORTION_4
-  PORTION_2[:sub_category][:sub_category] = PORTION_3
+  PORTION_2[:sub_category] = PORTION_3
   PORTION_1[:sub_category][:sub_category][:sub_category] = PORTION_2
 
   puts "Updated data, PORTION_4: #{PORTION_4}"
